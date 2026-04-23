c6511bccc3e264b3d64ac42e1d5bc2fe6be9ee5d
Henoc Elías Jorge Ugarte <henoceliasjorgeugarte@gmail.com>
2026-04-23 11:45:26 -0500

    fix(security): removed redundant arg

--- src/main/java/com/runaitec/credimacpato/security/WebSecurityConfig.java
+++ src/main/java/com/runaitec/credimacpato/security/WebSecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final JwtAuthorizationFilter jwtAuthorizationFilter;
